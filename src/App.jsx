f5bea6a60700dd41827570b5c388fa8007e69eba
wellington <wellingtonsoaresfe.jr@gmail.com>
2024-04-01 00:11:01 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Luxury from "./components/luxury"
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center itens-center">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans />
         <Suvs />
         <Luxury />
-      </section>
+      </div>
     </main>
   )
 }
