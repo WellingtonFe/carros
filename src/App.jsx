847fcc0f4c3c8333e3a76be0293481c765957baf
wellington <wellingtonsoaresfe.jr@gmail.com>
2024-03-31 23:33:50 -0300

    primeiro commit responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import iconSuvs from "./assets/icon-suvs.svg"
 import iconLuxury from "./assets/icon-luxury.svg"
 export default function App() {
   return (
-    <main className="min-h-svh bg-offwhitebg py-20 px-6">
-      <section>
+    <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center itens-center">
+      <section className="lg:flex">
         <div className="bg-orange p-12 rounded-t-lg">
           <img src={iconSedans} alt="icone dos carros sedans" className="mb-7"/>
-          <h2 className="font-shoulders mb-7 uppercase text-4-xl font-bold text-offwhitebg">Sedans</h2>
+          <h2 className="font-shoulders mb-7 uppercase text-4-xl font-bold text-offwhitebg ">Sedans</h2>
           <p className="font-lexend mb-7 text-offwhite">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/sedans" target="_blank" className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-orange ">Ver mais</a>
         </div>
